fc2ab36605cde69b65a6db3ddbbd624955b02c9e
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-02-11 15:24:21 +0200

    project architecture example
    project architecture example

---
+++ asp.net project architecture/HouseRentingSystem.Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+namespace HouseRentingSystem.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
